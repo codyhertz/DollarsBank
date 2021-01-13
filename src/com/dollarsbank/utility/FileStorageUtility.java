d330bbcae63324f29a650f3f2991261822c13489
codyhertz <codyhertz@yahoo.com>
2021-01-13 10:11:51 -0600

    user storage utility

--- src/com/dollarsbank/utility/FileStorageUtility.java
+++ src/com/dollarsbank/utility/FileStorageUtility.java
@@ -1,6 +1,56 @@
 package com.dollarsbank.utility;
 
+import java.util.ArrayList;
+
+import com.dollarsbank.model.Customer;
+
 public class FileStorageUtility
 {
+	private ArrayList<Customer> users = new ArrayList<Customer>();
+	private Customer currentUser;
 
+	public FileStorageUtility()
+	{
+		super();
+		currentUser = null;
+	}
+	
+	public void addUser(Customer cust)
+	{
+		this.users.add(cust);
+	}
+	
+	public boolean login(String userId, String password)
+	{
+		for(int i = 0; i < users.size(); i++)
+		{
+			if(users.get(i).getUserId().equals(userId))
+			{
+				boolean correctPassword = password == users.get(i).getPassword();
+				
+				if(correctPassword)
+				{
+					this.setCurrentUser(users.get(i));
+				}
+				
+				return correctPassword;
+			}
+		}
+		return false;
+	}
+	
+	public void logout()
+	{
+		setCurrentUser(null);
+	}
+
+	public Customer getCurrentUser()
+	{
+		return currentUser;
+	}
+
+	public void setCurrentUser(Customer currentUser)
+	{
+		this.currentUser = currentUser;
+	}
 }
